72f8eaead2fa4e884590d30dd44fe815be470a73
shinas <shinas@folkslogic.com>
2025-02-28 15:25:22 +0530

    Dialog tab -- code cleanups

---
+++ lib/config/constants/details_constants.dart
@@ -0,0 +1,6 @@
+class DetailsConstantValues{
+  static const String phoneNumber = "+918088670650";
+  static const String email = "shinasnajeerkoya@gmail.com";
+  static const String portfolioUrl = "https://shinasnajeerkoya.github.io/shinas_portfolio_website/";
+
+}
