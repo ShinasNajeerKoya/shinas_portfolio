3c76f5303ec7a6c6f049d917151c2dd3136817d7
ShinasNajeerKoya <shinasnajeerkoya@gmail.com>
2025-03-01 21:52:09 +0530

    #0002 feature (main layout)

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Portfolio',
+      title: 'My Portfolio', 
       debugShowCheckedModeBanner: false,
       // Hide the default debug banner
       theme: ThemeData(
