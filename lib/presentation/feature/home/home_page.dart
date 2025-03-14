72c6a08be192f59de64621999e819b289684b3f2
ShinasNajeerKoya <shinasnajeerkoya@gmail.com>
2025-03-14 09:41:48 +0530

    -- testing for mobile we layout

--- lib/presentation/feature/home/home_page.dart
+++ lib/presentation/feature/home/home_page.dart
@@ -1,30 +1,52 @@
+import 'dart:html' as html;
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:shinas_koya_portfolio/presentation/feature/home/mobile/mobile_home_screen.dart';
 import 'package:shinas_koya_portfolio/presentation/feature/splash/web_splash_page.dart';
+
+bool isMobileWeb() {
+  final userAgent = html.window.navigator.userAgent.toLowerCase();
+  return userAgent.contains("android") || userAgent.contains("iphone") || userAgent.contains("ipad");
+}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Widget homeScreen;
+    // Widget homeScreen;
+    //
+    // if (kIsWeb || Platform.isMacOS || Platform.isWindows) {
+    //   homeScreen = const WebSplashPage(); // Web Layout
+    //   // homeScreen = Provider<WebHomeBloc>(
+    //   //   create: (context) => provideWebHomeBloc(),
+    //   //   dispose: (context, bloc) => bloc.dispose(),
+    //   //   child: const WebHomePage(),
+    //   // ); // Web Layout
+    // } else {
+    //   homeScreen = const MobileHomeScreen(); // Mobile Layout
+    //
+    //   // homeScreen = const WebSplashPage();
+    // }
+    //
+    // return homeScreen;
 
-    if (kIsWeb || Platform.isMacOS || Platform.isWindows) {
-      homeScreen = const WebSplashPage(); // Web Layout
-      // homeScreen = Provider<WebHomeBloc>(
-      //   create: (context) => provideWebHomeBloc(),
-      //   dispose: (context, bloc) => bloc.dispose(),
-      //   child: const WebHomePage(),
-      // ); // Web Layout
-    } else {
-      // homeScreen = const MobileHomeScreen(); // Mobile Layout
-      /// temporary removing for aman
-      homeScreen = const WebSplashPage();
+    if (kIsWeb) {
+      if (isMobileWeb()) {
+        return const MobileHomeScreen(); // Show mobile layout for mobile browsers
+      } else {
+        return const WebSplashPage(); // Show desktop layout for desktop browsers
+      }
     }
-
-    return homeScreen;
+    
+    // For non-web platforms (Native Android, iOS, Windows, MacOS)
+    if (Platform.isMacOS || Platform.isWindows) {
+      return const WebSplashPage();
+    } else {
+      return const MobileHomeScreen();
+    }
   }
 }
 
